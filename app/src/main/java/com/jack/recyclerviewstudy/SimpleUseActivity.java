d71acbe24a59ef3f900da9f1e36fd84172b767cb
zhuangzi200323 <zhuangzi200323@163.com>
2021-01-22 15:23:48 +0800

    init

---
+++ app/src/main/java/com/jack/recyclerviewstudy/SimpleUseActivity.java
@@ -0,0 +1,14 @@
+package com.jack.recyclerviewstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimpleUseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_use);
+    }
+}
